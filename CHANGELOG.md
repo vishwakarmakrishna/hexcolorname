8f6db315d9e22d7744bd9275465b6b31ff44435c
kv0871916 <unknown0871916@gmail.com>
2022-02-06 04:59:44 +0530

    Examples added

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,3 +9,7 @@ Color Code Text Widget
 ## 0.0.3
 
 Color Code Text with Complementary & Inverted color
+
+## 0.0.4
+
+Color Code Text with Complementary & Inverted , TextInteverted  , Default styles with examples
---
+++ lib/example/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolorname/hexcolorname.dart';
+
+///
+/// complementary Mode will return the complementary color of the given color #C2ABA2
+/// inverted Mode will return the inverted color of the given color #5D463D
+/// textinverted Mode will return the Black / White color of the given color #000000 for eg. for light color with will return black and for dark color will return white
+///
+const yourColor = "A2B9C2";
+const String _colorString =
+    '0xFF$yourColor'; // or '#A2B9C2'.replaceAll('#', '0xFF');
+final Color _color = Color(int.parse(_colorString));
+void main() => runApp(
+      MaterialApp(
+        home: Material(
+          color: _color,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Text(
+                  "Style Mode of ColorCodeName:\n${(_color)}\nCadet Blue (Crayola)",
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Center(
+                child: ColorCodeName(
+                    style: TextStyle(
+                        //customize your text style here
+                        ),
+                    hexColor: _colorString),
+              ),
+              Center(
+                child: Text("Complementary color Mode of ColorCodeName "),
+              ),
+              Center(
+                child: ColorCodeName(
+                    mode: ColorMode.complementarycolor, hexColor: _colorString),
+              ),
+              Center(
+                child: Text("Inverted color Mode of ColorCodeName"),
+              ),
+              Center(
+                child: ColorCodeName(
+                    mode: ColorMode.invertedcolor, hexColor: _colorString),
+              ),
+              Center(
+                child: Text("TextInvert color Mode of ColorCodeName "),
+              ),
+              Center(
+                child: ColorCodeName(
+                    mode: ColorMode.textinvert, hexColor: _colorString),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
